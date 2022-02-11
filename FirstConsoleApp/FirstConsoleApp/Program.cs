87264a499ce802c9eaed38471c4e3f4d6ce57635
Lukasz Lipka <P2624016@my365.dmu.ac.uk>
2022-02-11 14:35:14 +0000

    First project

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
