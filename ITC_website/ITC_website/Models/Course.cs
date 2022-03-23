a074a35ab135d629f1573817002c7edf96502ad2
Dawid Korba <P2577974@my365.dmu.ac.uk>
2022-03-23 13:20:01 +0000

    added classes and attributes for them

---
+++ ITC_website/ITC_website/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITC_website.Models
+{
+    public class Course
+    {
+        public virtual int CourseId { get; set; }
+        public virtual string CourseName { get; set; }
+        public virtual string CurseDescription { get; set; }
+
+    }
+}
---
+++ ITC_website/ITC_website/Models/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITC_website.Models
+{
+    public class Staff
+    {
+        public virtual int StaffId { get; set; }
+        public virtual string StaffName { get; set; }
+        public virtual string StaffEmail { get; set; }
+        public virtual string StaffModule { get; set; }
+
+    }
+}
